2e26b638e490c49427c9c81ffb0bb1ef8f96ac2a
Ravin Kohli <kohliravin7@gmail.com>
2020-08-06 16:25:17 +0200

    Added RescalerChoice and test, bug in find_components

--- test/test_pipeline/test_rescalers.py
+++ test/test_pipeline/test_rescalers.py
@@ -19,11 +19,11 @@ class TestNormalizer(unittest.TestCase):
         X_train = preprocessor.transform(X[train_indices])
         X_test = preprocessor.transform(X[test_indices])
 
-        assert_allclose(X_train, np.array([[0.26726124, 0.53452248, 0.80178373],\
-                                                [0.45584231, 0.56980288, 0.68376346], \
+        assert_allclose(X_train, np.array([[0.26726124, 0.53452248, 0.80178373],
+                                                [0.45584231, 0.56980288, 0.68376346], 
                                                 [0.53806371, 0.57649683, 0.61492995]]))
-        assert_allclose(X_test, np.array([[0.50257071, 0.57436653, 0.64616234], \
-                                                [0.54471514, 0.5767572 , 0.60879927], \
+        assert_allclose(X_test, np.array([[0.50257071, 0.57436653, 0.64616234], 
+                                                [0.54471514, 0.5767572 , 0.60879927], 
                                                 [0.5280169 , 0.57601843, 0.62401997]]))
 
     def test_l1_norm(self):
@@ -38,11 +38,11 @@ class TestNormalizer(unittest.TestCase):
         X_train = preprocessor.transform(X[train_indices])
         X_test = preprocessor.transform(X[test_indices])
 
-        assert_allclose(X_train, np.array([[0.16666667, 0.33333333, 0.5], \
-                                            [0.26666667, 0.33333333, 0.4], \
+        assert_allclose(X_train, np.array([[0.16666667, 0.33333333, 0.5], 
+                                            [0.26666667, 0.33333333, 0.4],
                                             [0.31111111, 0.33333333, 0.35555556]]))
-        assert_allclose(X_test, np.array([[0.29166667, 0.33333333, 0.375     ],\
-                                            [0.31481481, 0.33333333, 0.35185185], \
+        assert_allclose(X_test, np.array([[0.29166667, 0.33333333, 0.375     ],
+                                            [0.31481481, 0.33333333, 0.35185185],
                                             [0.30555556, 0.33333333, 0.36111111]]))   
     def test_max_norm(self):
         X = np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6],
@@ -57,11 +57,11 @@ class TestNormalizer(unittest.TestCase):
         X_train = preprocessor.transform(X[train_indices])
         X_test = preprocessor.transform(X[test_indices])
 
-        assert_allclose(X_train, np.array([[0.33333333, 0.66666667, 1.        ],\
+        assert_allclose(X_train, np.array([[0.33333333, 0.66666667, 1.        ],
                                             [0.66666667, 0.83333333, 1.        ],
                                             [0.875     , 0.9375    , 1.        ]]))
-        assert_allclose(X_test, np.array([[0.77777778, 0.88888889, 1.        ],\
-                                            [0.89473684, 0.94736842, 1.        ],\
+        assert_allclose(X_test, np.array([[0.77777778, 0.88888889, 1.        ],
+                                            [0.89473684, 0.94736842, 1.        ],
                                             [0.84615385, 0.92307692, 1.        ]]))
 
    
@@ -79,11 +79,11 @@ class TestMinMaxScaler(unittest.TestCase):
         X_train = preprocessor.transform(X[train_indices])
         X_test = preprocessor.transform(X[test_indices])
 
-        assert_allclose(X_train, np.array([[0.        , 0.        , 0.        ],\
-                                        [0.23076923, 0.23076923, 0.23076923],\
+        assert_allclose(X_train, np.array([[0.        , 0.        , 0.        ],
+                                        [0.23076923, 0.23076923, 0.23076923],
                                         [1.        , 1.        , 1.        ]]))
 
-        assert_allclose(X_test, np.array([[0.46153846, 0.46153846, 0.46153846],\
+        assert_allclose(X_test, np.array([[0.46153846, 0.46153846, 0.46153846],
                                         [1.23076923, 1.23076923, 1.23076923],
                                         [0.76923077, 0.76923077, 0.76923077]]))
 
@@ -102,10 +102,10 @@ class TestStandardScaler(unittest.TestCase):
         X_train = preprocessor.transform(X[train_indices])
         X_test = preprocessor.transform(X[test_indices])
 
-        assert_allclose(X_train, np.array([[-0.95961623, -0.95961623, -0.95961623],\
-                                        [-0.4198321 , -0.4198321 , -0.4198321 ],\
+        assert_allclose(X_train, np.array([[-0.95961623, -0.95961623, -0.95961623],
+                                        [-0.4198321 , -0.4198321 , -0.4198321 ],
                                         [ 1.37944833,  1.37944833,  1.37944833]]))
 
-        assert_allclose(X_test, np.array([[0.11995203, 0.11995203, 0.11995203],\
-                                        [1.91923246, 1.91923246, 1.91923246],\
+        assert_allclose(X_test, np.array([[0.11995203, 0.11995203, 0.11995203],
+                                        [1.91923246, 1.91923246, 1.91923246],
                                         [0.8396642 , 0.8396642 , 0.8396642 ]]))
