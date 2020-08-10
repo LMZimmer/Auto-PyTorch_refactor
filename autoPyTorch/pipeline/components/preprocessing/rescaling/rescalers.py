9e927d1876b1bb07c55ca0c6958c89ed30f68379
Ravin Kohli <kohliravin7@gmail.com>
2020-08-10 17:42:52 +0200

    made flake and mypy suggestions

--- autoPyTorch/pipeline/components/preprocessing/rescaling/rescalers.py
+++ autoPyTorch/pipeline/components/preprocessing/rescaling/rescalers.py
@@ -1,8 +1,9 @@
 import ConfigSpace as CS
 import ConfigSpace.hyperparameters as CSH
 import numpy as np
-from sklearn.preprocessing import Normalizer as SklearnNormalizer, MinMaxScaler as SklearnMinMaxScaler, StandardScaler as SklearnStandardScaler
 from typing import Optional, Tuple, Union
+from sklearn.preprocessing import MinMaxScaler as SklearnMinMaxScaler, Normalizer as SklearnNormalizer
+from sklearn.preprocessing import StandardScaler as SklearnStandardScaler
 
 from autoPyTorch.pipeline.components.preprocessing.rescaling import BaseScaler
 
@@ -14,12 +15,12 @@ class Normalizer(BaseScaler):
     def __init__(self, random_state: Optional[int] = None, norm: str = 'l2'):
         self.random_state = random_state
         self.preprocessor = SklearnNormalizer(norm=norm, copy=False)
-    
+
     @staticmethod
     def get_hyperparameter_search_space(dataset_properties: Optional[dict] = None) -> CS.ConfigurationSpace:
         cs = CS.ConfigurationSpace()
         norm = CSH.CategoricalHyperparameter("norm", ["l1", "l2", "max"], default_value="l2")
-        cs.add_hyperparameter(strategy)
+        cs.add_hyperparameter(norm)
         return cs
 
 
@@ -27,7 +28,9 @@ class MinMaxScaler(BaseScaler):
     '''
     Scale numerical columns/features into feature_range
     '''
-    def __init__(self, random_state: Optional[int] = None, feature_range: Tuple[Union[int, float], Union[int, float]] = (0,1)):
+    def __init__(self, 
+                random_state: Optional[int] = None, 
+                feature_range: Tuple[Union[int, float], Union[int, float]]=(0, 1)):
         self.random_state = random_state
         self.preprocessor = SklearnMinMaxScaler(feature_range=feature_range, copy=False)
 
@@ -39,8 +42,8 @@ class StandardScaler(BaseScaler):
     def __init__(self, random_state: Optional[int] = None):
         self.random_state = random_state
         self.preprocessor = SklearnStandardScaler(with_mean=True, with_std=True, copy=False)
-    
-    
+
+
 class NoneScaler(BaseScaler):
     '''
     No scaling performed
@@ -56,4 +59,3 @@ class NoneScaler(BaseScaler):
     
     def fit_transform(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> np.ndarray:
         return self.fit(X, y).transform(X)
-    
