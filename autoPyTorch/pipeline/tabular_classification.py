23434dd183d9f7313b71ee37a07212022824a60e
Ravin Kohli <kohliravin7@gmail.com>
2020-08-15 18:50:02 +0200

    Added encoder and scaler choice to tabular pipeline

--- autoPyTorch/pipeline/tabular_classification.py
+++ autoPyTorch/pipeline/tabular_classification.py
@@ -8,9 +8,15 @@ from sklearn.base import ClassifierMixin
 
 from autoPyTorch.pipeline.base_pipeline import BasePipeline
 from autoPyTorch.pipeline.components.base_choice import autoPyTorchChoice
+<<<<<<< HEAD
 from autoPyTorch.pipeline.components.setup.lr_scheduler.base_scheduler_choice import SchedulerChoice
 from autoPyTorch.pipeline.components.setup.network.base_network_choice import NetworkChoice
 from autoPyTorch.pipeline.components.setup.optimizer.base_optimizer_choice import OptimizerChoice
+=======
+from autoPyTorch.pipeline.components.preprocessing.encoding import EncoderChoice
+from autoPyTorch.pipeline.components.preprocessing.scaling import ScalerChoice
+from autoPyTorch.pipeline.components.setup.lr_scheduler import SchedulerChoice
+>>>>>>> Added encoder and scaler choice to tabular pipeline
 
 
 class TabularClassificationPipeline(ClassifierMixin, BasePipeline):
@@ -180,10 +186,18 @@ class TabularClassificationPipeline(ClassifierMixin, BasePipeline):
             default_dataset_properties.update(dataset_properties)
 
         steps.extend([
+<<<<<<< HEAD
             ("network", NetworkChoice(default_dataset_properties)),
             ("optimizer", OptimizerChoice(default_dataset_properties)),
             ("lr_scheduler", SchedulerChoice(default_dataset_properties)),
             ])
+=======
+            ("Encoder", EncoderChoice(default_dataset_properties)),
+            ("Rescaler", ScalerChoice(default_dataset_properties)),
+            ("scheduler", SchedulerChoice(default_dataset_properties)),
+        ])
+
+>>>>>>> Added encoder and scaler choice to tabular pipeline
         return steps
 
     def _get_estimator_hyperparameter_name(self) -> str:
