627af97f0367fcb826b9c5a0b3d9e7003dca21aa
Ravin Kohli <kohliravin7@gmail.com>
2020-08-15 18:49:28 +0200

    added docstring

--- autoPyTorch/pipeline/components/preprocessing/base_preprocessor.py
+++ autoPyTorch/pipeline/components/preprocessing/base_preprocessor.py
@@ -8,16 +8,36 @@ from autoPyTorch.pipeline.components.base_component import autoPyTorchComponent
 
 
 class autoPyTorchPreprocessingAlgorithm(autoPyTorchComponent):
-    '''
-    Base class for preprocessing algorithms.
-    '''
+    """
+     Provides abstract interface for preprocessing algorithms in AutoPyTorch.
+    """
     def __init__(self) -> None:
         self.preprocessor: BaseEstimator = None
 
     def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None, **fit_params: Any) -> autoPyTorchComponent:
+        """
+        The fit function calls the fit function of the underlying model
+        and returns the transformed array.
+        Args:
+            X (np.ndarray): input features
+            y (Optional[np.ndarray]): input labels
+
+        Returns:
+            instance of self
+        """
         raise NotImplementedError
 
     def transform(self, X: np.ndarray) -> np.ndarray:
+        """
+        The transform function calls the transform function of the
+        underlying model and returns the transformed array.
+
+        Args:
+            X (np.ndarray): input features
+
+        Returns:
+            np.ndarray: Transformed features
+        """
         raise NotImplementedError
 
     def get_preprocessor(self) -> BaseEstimator:
