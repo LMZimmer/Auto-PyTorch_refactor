a0edc0dd4b69e7100f30641b6a885d94e922476a
Ravin Kohli <kohliravin7@gmail.com>
2020-08-26 14:33:51 +0200

    added __call__ and get_column_transformer to base_preprocessor

--- autoPyTorch/pipeline/components/preprocessing/base_preprocessor.py
+++ autoPyTorch/pipeline/components/preprocessing/base_preprocessor.py
@@ -3,32 +3,34 @@ from typing import Any, Dict, Optional, Union
 import numpy as np
 
 from sklearn.base import BaseEstimator
-from sklearn.base import TransformerMixin
+from sklearn.compose import ColumnTransformer
 
 import torch
 
 from autoPyTorch.pipeline.components.base_component import autoPyTorchComponent
 
 
-class autoPyTorchPreprocessingComponent(TransformerMixin, autoPyTorchComponent):
+class autoPyTorchPreprocessingComponent(autoPyTorchComponent):
     """
      Provides abstract interface for preprocessing algorithms in AutoPyTorch.
     """
-    def __init__(self) -> None:
-        self.preprocessor: Optional[BaseEstimator] = None
+    def __init__(self, random_state: Optional[Union[np.random.RandomState, int]] = None) -> None:
+        self.random_state = random_state
+        self.preprocessor: Union[Dict[str, BaseEstimator], Optional[BaseEstimator]] = None
+        self.column_transformer: Optional[ColumnTransformer] = None
 
     def fit(self, X: Dict[str, Any], y: Any = None) -> autoPyTorchComponent:
         """
         The fit function calls the fit function of the underlying model
         and returns the self.
         Args:
-            X (np.ndarray): input features
-            y (Optional[np.ndarray]): input labels
+            X (Dict[str, Any]): 'X' dictionary
+            y (Any): should be none
 
         Returns:
             instance of self
         """
-        raise NotImplementedError
+        raise NotImplementedError()
 
     def transform(self, X: Dict[str, Any]) -> Dict[str, Any]:
         """
@@ -39,7 +41,7 @@ class autoPyTorchPreprocessingComponent(TransformerMixin, autoPyTorchComponent):
         Returns:
             (Dict[str, Any]): the updated 'X' dictionary
         """
-        raise NotImplementedError
+        raise NotImplementedError()
 
     def __call__(self, X: Union[np.ndarray, torch.tensor]) -> Union[np.ndarray, torch.tensor]:
         """
@@ -52,13 +54,14 @@ class autoPyTorchPreprocessingComponent(TransformerMixin, autoPyTorchComponent):
         Returns:
             Union[np.ndarray, torch.tensor]: Transformed data tensor
         """
-        raise NotImplementedError
+        raise NotImplementedError()
 
-    def get_preprocessor(self) -> BaseEstimator:
+    def get_column_transformer(self) -> ColumnTransformer:
         """
-        Get the underlying sklearn preprocessor.
-        Can only be called if fit() has been called on the object.
+        Get fitted column transformer that is wrapped around
+        the sklearn preprocessor. Can only be called if fit()
+        has been called on the object.
         Returns:
-            BaseEstimator: Fitted sklearn preprocessor
+            BaseEstimator: Fitted sklearn column transformer
         """
-        return self.preprocessor
+        return self.column_transformer
