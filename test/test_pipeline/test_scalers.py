9e39ac3eebae3b7444c8bc5bd4373e9c3e796031
Ravin Kohli <kohliravin7@gmail.com>
2020-08-26 14:32:51 +0200

    Updated tests for preprocessors

--- test/test_pipeline/test_scalers.py
+++ test/test_pipeline/test_scalers.py
@@ -6,7 +6,7 @@ from numpy.testing import assert_allclose
 
 from sklearn.base import clone
 
-from autoPyTorch.pipeline.components.preprocessing.scaling import ScalerChoice
+from autoPyTorch.pipeline.components.preprocessing.scaling.base_scaler_choice import ScalerChoice
 from autoPyTorch.pipeline.components.preprocessing.scaling.MinMaxScaler import MinMaxScaler
 from autoPyTorch.pipeline.components.preprocessing.scaling.NoneScaler import NoneScaler
 from autoPyTorch.pipeline.components.preprocessing.scaling.Normalizer import Normalizer
@@ -39,130 +39,216 @@ class TestNormalizer(unittest.TestCase):
             self.assertEqual(param1, param2)
 
     def test_l2_norm(self):
-        X = np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6],
-                     [11, 12, 13], [17, 18, 19], [14, 15, 16]])
+        data = np.array([[1, 2, 3],
+                      [7, 8, 9],
+                      [4, 5, 6],
+                      [11, 12, 13],
+                      [17, 18, 19],
+                      [14, 15, 16]])
         train_indices = np.array([0, 2, 5])
         test_indices = np.array([1, 4, 3])
+        categorical_columns = list()
+        numerical_columns = [0, 1, 2]
+        X = {
+            'train': data[train_indices],
+            'categorical_columns': categorical_columns,
+            'numerical_columns': numerical_columns,
+        }
+        scaler_component = Normalizer(norm='l2')
 
-        preprocessor = Normalizer(norm='l2')
+        scaler_component = scaler_component.fit(X)
+        X = scaler_component.transform(X)
 
-        preprocessor = preprocessor.fit(X[train_indices])
-        X_train = preprocessor.transform(X[train_indices])
-        X_test = preprocessor.transform(X[test_indices])
+        # check if scaler added to X is instance of self
+        self.assertEqual(X['scaler'], scaler_component)
 
-        assert_allclose(X_train, np.array([[0.26726124, 0.53452248, 0.80178373],
-                                          [0.45584231, 0.56980288, 0.68376346],
-                                          [0.53806371, 0.57649683, 0.61492995]]))
-        assert_allclose(X_test, np.array([[0.50257071, 0.57436653, 0.64616234],
+        transformed = scaler_component(data[test_indices])
+
+        assert_allclose(transformed, np.array([[0.50257071, 0.57436653, 0.64616234],
                                          [0.54471514, 0.5767572, 0.60879927],
                                          [0.5280169, 0.57601843, 0.62401997]]))
 
     def test_l1_norm(self):
-        X = np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6],
-                     [11, 12, 13], [17, 18, 19], [14, 15, 16]])
+        data = np.array([[1, 2, 3],
+                      [7, 8, 9],
+                      [4, 5, 6],
+                      [11, 12, 13],
+                      [17, 18, 19],
+                      [14, 15, 16]])
         train_indices = np.array([0, 2, 5])
         test_indices = np.array([1, 4, 3])
+        categorical_columns = list()
+        numerical_columns = [0, 1, 2]
+        X = {
+            'train': data[train_indices],
+            'categorical_columns': categorical_columns,
+            'numerical_columns': numerical_columns,
+        }
+        scaler_component = Normalizer(norm='l1')
 
-        preprocessor = Normalizer(norm='l1')
+        scaler_component = scaler_component.fit(X)
+        X = scaler_component.transform(X)
 
-        preprocessor = preprocessor.fit(X[train_indices])
-        X_train = preprocessor.transform(X[train_indices])
-        X_test = preprocessor.transform(X[test_indices])
+        # check if scaler added to X is instance of self
+        self.assertEqual(X['scaler'], scaler_component)
+        transformed = scaler_component(data[test_indices])
 
-        assert_allclose(X_train, np.array([[0.16666667, 0.33333333, 0.5],
-                                          [0.26666667, 0.33333333, 0.4],
-                                          [0.31111111, 0.33333333, 0.35555556]]))
-        assert_allclose(X_test, np.array([[0.29166667, 0.33333333, 0.375],
-                                         [0.31481481, 0.33333333, 0.35185185],
-                                         [0.30555556, 0.33333333, 0.36111111]]))
+        assert_allclose(transformed, np.array([[0.29166667, 0.33333333, 0.375],
+                                          [0.31481481, 0.33333333, 0.35185185],
+                                          [0.30555556, 0.33333333, 0.36111111]]))
 
     def test_max_norm(self):
-        X = np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6],
-                     [11, 12, 13], [17, 18, 19], [14, 15, 16]])
+        data = np.array([[1, 2, 3],
+                      [7, 8, 9],
+                      [4, 5, 6],
+                      [11, 12, 13],
+                      [17, 18, 19],
+                      [14, 15, 16]])
         train_indices = np.array([0, 2, 5])
         test_indices = np.array([1, 4, 3])
+        categorical_columns = list()
+        numerical_columns = [0, 1, 2]
+        X = {
+            'train': data[train_indices],
+            'categorical_columns': categorical_columns,
+            'numerical_columns': numerical_columns,
+        }
+        scaler_component = Normalizer(norm='max')
 
-        preprocessor = Normalizer(norm='max')
+        scaler_component = scaler_component.fit(X)
+        X = scaler_component.transform(X)
 
-        preprocessor = preprocessor.fit(X[train_indices])
-        X_train = preprocessor.transform(X[train_indices])
-        X_test = preprocessor.transform(X[test_indices])
+        # check if scaler added to X is instance of self
+        self.assertEqual(X['scaler'], scaler_component)
+        transformed = scaler_component(data[test_indices])
 
-        assert_allclose(X_train, np.array([[0.33333333, 0.66666667, 1],
-                                          [0.66666667, 0.83333333, 1],
-                                          [0.875, 0.9375, 1]]))
-        assert_allclose(X_test, np.array([[0.77777778, 0.88888889, 1],
+        assert_allclose(transformed, np.array([[0.77777778, 0.88888889, 1],
                                           [0.89473684, 0.94736842, 1],
                                           [0.84615385, 0.92307692, 1]]))
+
+    def test_l2_norm(self):
+        data = np.array([[1, 2, 3],
+                      [7, 8, 9],
+                      [4, 5, 6],
+                      [11, 12, 13],
+                      [17, 18, 19],
+                      [14, 15, 16]])
+        train_indices = np.array([0, 2, 5])
+        test_indices = np.array([1, 4, 3])
+        categorical_columns = list()
+        numerical_columns = [0, 1, 2]
+        X = {
+            'train': data[train_indices],
+            'categorical_columns': categorical_columns,
+            'numerical_columns': numerical_columns,
+        }
+        scaler_component = Normalizer(norm='l2')
+
+        scaler_component = scaler_component.fit(X)
+        X = scaler_component.transform(X)
+
+        # check if scaler added to X is instance of self
+        self.assertEqual(X['scaler'], scaler_component)
+        transformed = scaler_component(data[test_indices])
+
+        assert_allclose(transformed, np.array([[0.50257071, 0.57436653, 0.64616234],
+                                         [0.54471514, 0.5767572, 0.60879927],
+                                         [0.5280169, 0.57601843, 0.62401997]]))
 
 
 class TestMinMaxScaler(unittest.TestCase):
 
     def test_minmax_scaler(self):
-        X = np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6],
-                     [11, 12, 13], [17, 18, 19], [14, 15, 16]])
+        data = np.array([[1, 2, 3],
+                      [7, 8, 9],
+                      [4, 5, 6],
+                      [11, 12, 13],
+                      [17, 18, 19],
+                      [14, 15, 16]])
         train_indices = np.array([0, 2, 5])
         test_indices = np.array([1, 4, 3])
+        categorical_columns = list()
+        numerical_columns = [0, 1, 2]
+        X = {
+            'train': data[train_indices],
+            'categorical_columns': categorical_columns,
+            'numerical_columns': numerical_columns,
+        }
+        scaler_component = MinMaxScaler()
 
-        preprocessor = MinMaxScaler()
+        scaler_component = scaler_component.fit(X)
+        X = scaler_component.transform(X)
 
-        preprocessor = preprocessor.fit(X[train_indices])
-        X_train = preprocessor.transform(X[train_indices])
-        X_test = preprocessor.transform(X[test_indices])
+        # check if scaler added to X is instance of self
+        self.assertEqual(X['scaler'], scaler_component)
 
-        assert_allclose(X_train, np.array([[0, 0, 0],
-                                          [0.23076923, 0.23076923, 0.23076923],
-                                          [1, 1, 1]]))
-
-        assert_allclose(X_test, np.array([[0.46153846, 0.46153846, 0.46153846],
-                                         [1.23076923, 1.23076923, 1.23076923],
-                                         [0.76923077, 0.76923077, 0.76923077]]))
+        transformed = scaler_component(data[test_indices])
+        assert_allclose(transformed, np.array([[0.46153846, 0.46153846, 0.46153846],
+                                          [1.23076923, 1.23076923, 1.23076923],
+                                          [0.76923077, 0.76923077, 0.76923077]]))
 
 
 class TestStandardScaler(unittest.TestCase):
 
     def test_standard_scaler(self):
-        X = np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6],
-                     [11, 12, 13], [17, 18, 19],
-                     [14, 15, 16]])
+        data = np.array([[1, 2, 3],
+                      [7, 8, 9],
+                      [4, 5, 6],
+                      [11, 12, 13],
+                      [17, 18, 19],
+                      [14, 15, 16]])
         train_indices = np.array([0, 2, 5])
         test_indices = np.array([1, 4, 3])
+        categorical_columns = list()
+        numerical_columns = [0, 1, 2]
+        X = {
+            'train': data[train_indices],
+            'categorical_columns': categorical_columns,
+            'numerical_columns': numerical_columns,
+        }
+        scaler_component = StandardScaler()
 
-        preprocessor = StandardScaler()
+        scaler_component = scaler_component.fit(X)
+        X = scaler_component.transform(X)
 
-        preprocessor = preprocessor.fit(X[train_indices])
-        X_train = preprocessor.transform(X[train_indices])
-        X_test = preprocessor.transform(X[test_indices])
+        # check if scaler added to X is instance of self
+        self.assertEqual(X['scaler'], scaler_component)
 
-        assert_allclose(X_train, np.array([[-0.95961623, -0.95961623, -0.95961623],
-                                          [-0.4198321, -0.4198321, -0.4198321],
-                                          [1.37944833, 1.37944833, 1.37944833]]))
+        transformed = scaler_component(data[test_indices])
 
-        assert_allclose(X_test, np.array([[0.11995203, 0.11995203, 0.11995203],
-                                         [1.91923246, 1.91923246, 1.91923246],
-                                         [0.8396642, 0.8396642, 0.8396642]]))
+        assert_allclose(transformed, np.array([[0.11995203, 0.11995203, 0.11995203],
+                                          [1.91923246, 1.91923246, 1.91923246],
+                                          [0.8396642, 0.8396642, 0.8396642]]))
 
 
 class TestNoneScaler(unittest.TestCase):
 
     def test_none_scaler(self):
-        X = np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6],
-                     [11, 12, 13], [17, 18, 19],
-                     [14, 15, 16]])
+        data = np.array([[1, 2, 3],
+                      [7, 8, 9],
+                      [4, 5, 6],
+                      [11, 12, 13],
+                      [17, 18, 19],
+                      [14, 15, 16]])
         train_indices = np.array([0, 2, 5])
         test_indices = np.array([1, 4, 3])
-        X_train = X[train_indices]
-        X_test = X[test_indices]
+        categorical_columns = list()
+        numerical_columns = [0, 1, 2]
+        X = {
+            'train': data[train_indices],
+            'categorical_columns': categorical_columns,
+            'numerical_columns': numerical_columns,
+        }
+        scaler_component = NoneScaler()
 
-        preprocessor = NoneScaler()
+        scaler_component = scaler_component.fit(X)
+        X = scaler_component.transform(X)
 
-        preprocessor = preprocessor.fit(X[train_indices])
-        X_train_trans = preprocessor.transform(X[train_indices])
-        X_test_trans = preprocessor.transform(X[test_indices])
+        # check if scaler added to X is instance of self
+        self.assertEqual(X['scaler'], scaler_component)
+        transformed = scaler_component(data[test_indices])
 
-        assert_allclose(X_train, X_train_trans)
-
-        assert_allclose(X_test, X_test_trans)
+        assert_allclose(transformed, transformed)
 
 
 class TestRescalerChoice(unittest.TestCase):
